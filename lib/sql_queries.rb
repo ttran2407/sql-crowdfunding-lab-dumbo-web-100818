754e54f77e7b99d8ee11e2401a7cad50f3997004
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 01:17:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "Write your SQL query Here"
 "SELECT projects.title, SUM(pledges.amount) FROM projects   INNER JOIN pledges ON projects.id = pledges.id
     WHERE SUM(pledges.amount) > projects.funding_goal
-    GROUP BY projects.title;
+    ;
 "
 end
 
