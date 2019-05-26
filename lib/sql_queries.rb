b95255475e168606b01cc4c6fb2f90b922aa6598
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 01:45:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
-"SELECT projects.title, SUM (pledges.amount)
+"SELECT projects.title, pledges.amount
  FROM projects
  INNER JOIN pledges ON projects.id = pledges.project_id
  "
