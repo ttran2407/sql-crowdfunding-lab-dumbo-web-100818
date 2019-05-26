8d1d52574b7690638b4fa68209ae4badc1dea053
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 00:29:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
-INS
+INSERT INTO projects (title) VALUES ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1") ("Mission 1");
+
+INSERT INTO users (name) VALUES ("Tien") ("Tien") ("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien")("Tien");
+
+INSERT INTO pledges (amount) VALUES (5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)(5000)
