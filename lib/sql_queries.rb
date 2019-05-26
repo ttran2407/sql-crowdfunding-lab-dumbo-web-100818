f84aecfe8cfabfd046d81b7a00589c34dbe1bea5
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 00:50:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,9 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "Write your SQL query Here"
-"SELECT name, age FROM "
+"SELECT users.name, users.age, pledge.amount FROM users
+    INNER JOIN pledges ON user.id = pledges.user_id
+    ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
