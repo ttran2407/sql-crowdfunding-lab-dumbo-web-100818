6cdd3524663cda674f9053a706289b043ed77687
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 00:55:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "Write your SQL query Here"
 "SELECT users.name, users.age, pledges.amount FROM users
-    INNER JOIN pledges ON user.id = pledges.user_id
+    INNER JOIN pledges ON users.id = pledges.user_id
     ORDER BY users.name;"
 end
 
