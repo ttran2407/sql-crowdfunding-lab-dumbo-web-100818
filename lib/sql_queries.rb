2c142f80fa8075526e39dfa917281301a9eea895
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 01:41:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "Write your SQL query Here"
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
  FROM projects 
  INNER JOIN pledges ON projects.id = pledges.project_id
  WHERE projects.category = 'music';"
