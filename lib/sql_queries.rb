c84bfd38ac184f01b6b01c0fbb04b4537d54204a
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 00:40:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
-"SELECT "
+"SELECT title FROM projects "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
